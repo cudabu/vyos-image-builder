f4a113e5b6d3bfa593aadaba279ffa2774fc619d
Curtis Bunch <cbunch@gmail.com>
2022-06-20 09:53:15 -0500

    minor fixes

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ HOST_FILE=$SOURCE_DIR/hosts
 EXTRA_ARGS="-e cloud_init=$VYOS_CLOUD_INIT -e guest_agent=$VYOS_GUEST_AGENT -e disk_size=$VYOS_DISK_SIZE -e enable_ssh=$VYOS_ENABLE_SSH"
 
 # execute ansible playbook to build qemu image
-echo "Starting log" >> $LOGFILE
+echo "Starting log" > $LOGFILE
 ansible-playbook $PLAYBOOK_FILE -i $HOST_FILE $EXTRA_ARGS >> $LOGFILE
 
 # get the created file and save to $OUTPUT_DIR
